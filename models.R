0aa9d75f48d6523b0d774dbd6f9e6aca5c0e03c3
TumeloS <tumelo@aims.ac.za>
2022-05-06 11:24:19 +0200

    new cm simulations and plots

--- models.R
+++ models.R
@@ -1,22 +1,28 @@
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(readxl)
 
-# # Read Parameter values for SARS Cov 2.
 
-pp <- read_excel("sars_cov2_par.xlsx", sheet = "sars_cov2_par")
+# Source the functions script
+
+source("./functions.R", local = TRUE)
+
+# Read Parameter values for SARS Cov 2.
+
+pp <- read_excel("./ebola_par.xlsx", sheet = "ebola_par")
 
 # Extract only values from the Data Frame
 
 parm_values <- pp$Value
 names(parm_values) <- pp$Code
-parm_values <- parm_values[1:13]
+parm_values <- parm_values[1:11]
 
 ## State variables
 
 N0 <- 150000 # Assumed total population
 
-ss <- read_excel("states.xlsx", sheet = "states")
+ss <- read_excel("./states.xlsx", sheet = "states")
 
 # Extract only values from the Data Frame
 
@@ -27,74 +33,62 @@ names(state_var) <- ss$Code
 
 time_seq <- seq(0, 365, 1)
 
-## Parameter values.
+library(dplyr)
 
-parm_values <- c(alpha = 1/6,     # 1/duration of pre-symptomatic = 1/6 days
-                 eta = 1/7,       # 1/duration of asymptomatic to recover = 1/7 days
-                 sigma = 1/5,     # NB: for now (NCEM)
-                 epsilon = 1/3,   # 1/duration of infection to hospitalized = 1/3 days
-                 #mu = 1/12,       # progression rate from H_T(d): 1/12 days
-                 nu = 1/12,       # 1/duration from not hospital to death = 1/15 days
-                 gamma = 1/18,    # 1/18 days=1/duration from hospital to recovered
-                 pa = 0.75,        # prob of remaining asymptomatic 75% (PHIRST-C)
-                 pm = 0.95,       # prob of being mild or moderate given symptomatic 90% (NCEM)
-                 #ps = 0.25,       # prob of survival given hospitalization (DATCOV)
-                 #a = 10,          # Hospital capacity constrain:-(cc)
-                 b = 1,
-                 pmax = 0.4,      # Maximum probability of being hospitalized in cc
-                 # 40% proportion of severe ILI cases.
-                 brn = 2,          # Reproduction number
-                 D = 5)           # Duration of infection in days
+# Source the inputs
 
-
-## State variables
-
-N0 = 150000  # Assumed total population
-
-state_var <- c(S = N0-1,
-               Ia = 1,
-               Is = 0,
-               M = 0,  
-               V = 0,
-               Nt = 0,
-               Hts = 0,
-               Htd = 0,  
-               R = 0,
-               CM = 0,
-               CA = 0)
-
-## Time interval
-
-time_seq <- seq(0, 365, 1)
+#source("./scripts/inputs.R", local = TRUE)
 
 ## Run the model to calculate cumulative
 
-CM1 <- run_covid(t = time_seq,
-                 pp = parm_values,
-                 vals =  c(ps = 0.25,
-                           du = 12,
-                           cc = 10),
-                 statev = state_var)
+CM1 <- run_covid(
+  t = time_seq,
+  pp = parm_values,
+  vals = c(
+    ps = 0.25,
+    du = 12,
+    cc = 20
+  ),
+  statev = state_var
+)
 
 ## We define new parameters with different values of
 ## ps, a and du.
 
 ## expand.grid helps to create the data with matching number of length.
 
-new_vals <- expand.grid(ps = seq(0, 1, 0.05),
-                        du = 8:30,
-                        cc = seq(20, 300, 20))
+new_vals <- expand.grid(
+  ps = seq(0, 1, 0.01),
+  du = 8:30,
+  cc =  seq(20, 300, 20)
+)
+
+# Run the model to produce all rates from the COVID
+
+out <- run_covid(
+  t = time_seq,
+  pp = parm_values,
+  vals = c(ps=.75, du = 15, cc = 20),
+  statev = state_var, ret_cm = FALSE
+)
+
+# Save simulation output
+
+
 
 ## Here we only run with 1 rows from data frame new_vals.
 
-run_covid(t = time_seq,
-          pp = parm_values,
-          vals =  new_vals[1,],
-          statev = state_var)
+run_covid(
+  t = time_seq,
+  pp = parm_values,
+  vals = new_vals[3, ],
+  statev = state_var
+)
 
 ## Calculating Cumulative Mortality for all values of (new_vals)
-if (file.exists("cm_res.RData")) {
-  load("cm_res.RData")
+
+if (file.exists("./cm_res.RData")) {
+  load("./cm_res.RData")
 } else {
   cm_res <- sapply(1:nrow(new_vals), function(ii) {
     run_covid(
@@ -104,12 +98,87 @@ if (file.exists("cm_res.RData")) {
       statev = state_var
     )
   })
-  save(cm_res, file = "cm_res.RData")
+  save(cm_res, file = "./cm_res.RData")
 }
 
-## Combine used parameters with corresponding cm value.
+
+
+## Combine used parameters with corresponding cm value and save it
 
 new_df <- cbind(new_vals, cm = cm_res)
+
+save(new_df, file = "./new_df.RData")
+
+# Plot the simulation output 
+# Make the data(out) long and plot according to the states.
+
+new_out <- out %>%
+  pivot_longer(S:CA, values_to = "Count", names_to = "States")
+
+model_out <- (ggplot(new_out)
+              +
+                geom_line(aes(
+                  x = time,
+                  y = Count,
+                  col = States
+                ), size = 1)
+              +
+                labs(
+                  x = "Time(days)"
+                  , y = "Population"
+                  , title = "Probability of surviving is 75%, 12 days of hospitalization 
+                    with 20 availabe beds in population of 150000 individuals."
+                )
+              +
+                facet_wrap(~States, scales = "free") 
+              +
+                ggeasy::easy_center_title()
+              +
+                theme_bw()
+              
+)
+
+print(model_out)
+
+
+
+########################################################
+# Hypothetical drugs
+
+bed_capacity <- 20
+
+baseline_cm <- function(bed_cap, psb = .75, dub = 10) new_df %>%
+  filter(ps == psb, du == dub, cc == bed_cap)
+
+plot1 <- (ggplot()
+          +
+            aes(x = ps, y = du, z = cm, color = as.character(cc)
+            )
+          +
+            geom_contour(breaks = c(min(new_df$cm), baseline_cm(bed_capacity)$cm, max(new_df$cm)), data = new_df[new_df$cc == bed_capacity, ])
+          +
+            geom_contour(breaks = c(min(new_df$cm), baseline_cm(180)$cm, max(new_df$cm)), data = new_df[new_df$cc == 180, ])
+          +
+            geom_point(data = baseline_cm(bed_capacity), size = 3, col = "blue")
+          +
+            lims(x = c(0, 1))
+          +
+            geom_vline(aes(xintercept = ps), data = baseline_cm(bed_capacity))
+          +
+            geom_hline(aes(yintercept = du), data = baseline_cm(bed_capacity))
+          +
+            labs(#title = "Cumulative mortality at given number of beds",
+              x = "Probablilty of surving"
+              , y = "Hospital duration (days)"
+              , color = "Number of beds")
+          + 
+            theme_bw()
+          +
+            ggeasy::easy_center_title()
+          
+)
+
+
 #######################################################
 #filter according to the know drug base line
 # We have duration of stay and probability of surviving
@@ -147,7 +216,7 @@ baseline_cm <- function(bed_cap, psb = .75, dub = 15 )new_df %>%
   + lims(x = c(0,1))
   + geom_point(data = baseline_cm(bed_capacity), size = 2, col = "red")
   + geom_hline(aes(yintercept = du), data = baseline_cm(bed_capacity))
-  #+ geom_vline(aes(xintercept = ps), data = baseline_cm(bed_capacity))
+  + geom_vline(aes(xintercept = ps), data = baseline_cm(bed_capacity))
 )
 
 #######################################
